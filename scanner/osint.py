603510a0a953f70a50a9063624f1b6dbf8d1adc3
57arl1ght <pborysevych@gmail.com>
2026-04-09 21:51:39 +0300

    Update osint.py

--- scanner/osint.py
+++ scanner/osint.py
@@ -9,13 +9,13 @@ def extract_emails(url):
     try:
         response = requests.get(url, timeout=5)
         
-        # Класичний регулярний вираз для пошуку email-адрес
+        
         email_pattern = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
         
-        # Знаходимо всі збіги та видаляємо дублікати (за допомогою set)
+        
         found_emails = set(re.findall(email_pattern, response.text))
         
-        # Фільтруємо хибні спрацьовування (наприклад, картинки типу name@2x.png)
+        
         valid_emails = [email for email in found_emails if not email.endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp'))]
         
         return list(valid_emails)
